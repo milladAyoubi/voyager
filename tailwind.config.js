a118d1f134d695f81faa23f7b02a88fa29968f1c
Millad Ayoubi <Milladayoubi@gmail.com>
2023-09-08 20:10:11 -0400

    Intial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        container: {
+            padding: {
+                DEFAULT: '15px',
+            },
+        },
+
+        screens: {
+            sm: '500px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1384px'
+        },
+
+        extend: {
+            colors: {
+                primary: '#101828',
+                seconday: '#667085',
+                accent: {
+                    DEFAULT: '#ed1d24',
+                    hover: '#dd242a'
+                },
+                body: '#dedede',
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
