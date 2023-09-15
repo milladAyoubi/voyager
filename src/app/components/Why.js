995480692f766b431b33a5e685e0ccd815917088
Millad Ayoubi <Milladayoubi@gmail.com>
2023-09-15 16:06:50 -0400

    Why Section Update

--- src/app/components/Why.js
+++ src/app/components/Why.js
@@ -1,10 +1,72 @@
+"use client"
+
+import Image from "next/image"
 import React from 'react'
+
+import {motion} from 'framer-motion'
+
+import {fadeIn} from './varients'
+
+import {MdHandshake, MdKey, MdTrendingUp} from 'react-icons/md'
 
 
 export default function Why() {
   return (
     <section className='section flex items-center bg-green' id='why'>
-
+      <div className="container mx-auto">
+        <motion.h2 
+        variants={fadeIn('up', 0.4)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.6}}
+        className="h2 text-center">Unmatched Excellence and Customer Satisfaction
+        </motion.h2>
+        <motion.p 
+        variants={fadeIn('up', 0.6)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.6}}
+        className="max-w-[680px] text-center mx-auto mb-2">
+          Our dedication to providing exceptional services sets us apart from the compitition. From the moment you engage with us,
+          we strive to exceed your expectations in every interaction. 
+        </motion.p>
+        <motion.div 
+        variants={fadeIn('up', 0.8)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.6}}
+        className='hidden md:flex justify-center mb-6 xl:mb-2'>
+          <Image src={'/images/why/car.svg'} width={1060} height={420} alt=''/>
+        </motion.div>
+        <div 
+        
+        className="flex flex-wrap justify-center xl:grid xl:grid-cols-3 gap-4 xl:gap-y-0 xl:gap-x-4">
+          <div className="flex flex-col items-center text-center max-w-[160px] xl:max-w-none p2 xl:p-0">
+              <MdKey className="text-[38px] text-accent mb-4"/>
+              <h3 className="h3">Quick & Easy</h3>
+              <p className="hidden xl:flex">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+          </div>
+          <div className="flex flex-col items-center text-center max-w-[160px] xl:max-w-none p2 xl:p-0">
+              <MdTrendingUp className="text-[38px] text-accent mb-4"/>
+              <h3 className="h3">Modern Inventory</h3>
+              <p className="hidden xl:flex">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+          </div>
+          <div className="flex flex-col items-center text-center max-w-[160px] xl:max-w-none p2 xl:p-0">
+              <MdHandshake className="text-[38px] text-accent mb-4"/>
+              <h3 className="h3">Flexible Services</h3>
+              <p className="hidden xl:flex">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Header from "../app/components/Header"
 import Hero from "../app/components/Hero"
 import Cars from './components/Cars'
 import About from './components/About'
-
+import Why from './components/Why'
 
 export default function Home() {
   return (
@@ -12,6 +12,8 @@ export default function Home() {
         <Hero/>
         <Cars/>
         <About/>
+        <Why/>
+        
         <div className='h-[4000px]'></div>
     </main>
   )
